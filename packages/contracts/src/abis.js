e9d722964b29bc9fdec16c1a134c1ca237f54102
Sawit Trisirisatayawong <sawit.tr@gmail.com>
2021-09-06 17:06:36 +0700

    bug: re-add contracts folder

---
+++ packages/contracts/src/abis.js
@@ -0,0 +1,7 @@
+import rarityAbi from "./abis/rarity.json";
+
+const abis = {
+  rarity: rarityAbi,
+};
+
+export default abis;
---
+++ packages/contracts/src/addresses.js
@@ -0,0 +1,7 @@
+// This address points to a dummy ERC20 contract deployed on Ethereum Mainnet,
+// Goerli, Kovan, Rinkeby and Ropsten. Replace it with your smart contracts.
+const addresses = {
+  rarity: "0xce761D788DF608BD21bdd59d6f4B54b2e27F25Bb",
+};
+
+export default addresses;
